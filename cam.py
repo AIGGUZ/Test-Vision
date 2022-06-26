d43221fcf36385ef2b203f67a627fa9bf2aab580
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-06-26 00:52:44 -0700

    Update cam.py

--- cam.py
+++ cam.py
@@ -10,7 +10,7 @@ st.title('Webcam Test')
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
 webCamFeed = True
-cap =  cv2.VideoCapture(1)
+cap =  cv2.VideoCapture(0)
 cap.set(10,160)
 heightImg = 700
 widthImg  = 700
@@ -64,6 +64,7 @@ while run:
             imgThresh = cv2.threshold(imgWarpGray, 170, 255,cv2.THRESH_BINARY_INV )[1] # APPLY THRESHOLD AND INVERSE
 
             boxes = af.splitBoxes(imgThresh) # GET INDIVIDUAL BOXES
+            #cv2.imshow("Split Test ", boxes[3])
             countR=0
             countC=0
             myPixelVal = np.zeros((questions,choices)) # TO STORE THE NON ZERO VALUES OF EACH BOX
@@ -112,23 +113,32 @@ while run:
             imgFinal = cv2.addWeighted(imgFinal, 1, imgInvGradeDisplay, 1,0)
 
             # IMAGE ARRAY FOR DISPLAY
+            imageArray = ([img,imgGray,imgCanny,imgContours],
+                          [imgBigContour,imgThresh,imgWarpColored,imgFinal])
             #cv2.imshow("Final Result", imgFinal)
     except:
-        imageArray = imgFinal
+        imageArray = ([img,imgGray,imgCanny,imgContours],
+                      [imgBlank, imgBlank, imgBlank, imgBlank])
 
+    # LABELS FOR DISPLAY
+    lables = [["Original","Gray","Edges","Contours"],
+              ["Biggest Contour","Threshold","Warpped","Final"]]
 
-    #cv2.imshow("Result",imgFinal)
+    stackedImage = af.stackImages(imageArray,0.5,lables)
+    #cv2.imshow("Result",stackedImage)
+
 
     # SAVE IMAGE WHEN 's' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('s'):
         cv2.imwrite("Scanned/myImage"+str(count)+".jpg",imgFinal)
-        cv2.rectangle(imgFinal,
+        cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
                       (1100, 350), (0, 255, 0), cv2.FILLED)
-        cv2.putText(imgFinal, "Scan Saved",
+        cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
                     cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
-        cv2.imshow('Result', imgFinal)
+        #cv2.imshow('Result', stackedImage)
         cv2.waitKey(300)
         count += 1
+
     imgFinal = cv2.cvtColor(imgFinal, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(imgFinal)
 else:
